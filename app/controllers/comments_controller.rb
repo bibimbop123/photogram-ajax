efcc13c5c695b5b1791255fad9783fde9eb60fe4
Brian Kim <brianhkim13@gmail.com>
2025-01-01 13:48:04 +0000

    ajaxify comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,6 +33,10 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        respond_to do |format|
+        # Handle JSON and HTML formats as usual
+        format.js
+      end
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -59,6 +63,10 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
+
+      format.js do
+        render template: "comments/destroy"
+      end
     end
   end
 
